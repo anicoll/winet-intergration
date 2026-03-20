cfb0e554798cf32d82c1d73b972f9183120f8559
anicoll <anicoll01@gmail.com>
2026-03-20 15:58:29 +1030

    refactor: DB refactoring

--- internal/pkg/database/write.go
+++ internal/pkg/database/write.go
@@ -2,15 +2,15 @@ package database
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgtype"
 
 	dbq "github.com/anicoll/winet-integration/internal/pkg/database/db"
 	"github.com/anicoll/winet-integration/internal/pkg/model"
-	"github.com/anicoll/winet-integration/internal/pkg/publisher"
 )
 
-func (d *Database) Write(ctx context.Context, data []publisher.DataPoint) error {
+func (d *Database) Write(ctx context.Context, data []map[string]any) error {
 	tx, err := d.pool.Begin(ctx)
 	if err != nil {
 		return err
@@ -18,13 +18,13 @@ func (d *Database) Write(ctx context.Context, data []publisher.DataPoint) error 
 	defer tx.Rollback(ctx)
 
 	qtx := d.queries.WithTx(tx)
-	for _, dp := range data {
+	for _, record := range data {
 		if _, err := qtx.InsertProperty(ctx, dbq.InsertPropertyParams{
-			TimeStamp:         dp.Timestamp,
-			UnitOfMeasurement: dp.UnitOfMeasurement,
-			Value:             dp.Value,
-			Identifier:        dp.Identifier,
-			Slug:              dp.Slug,
+			TimeStamp:         record["timestamp"].(time.Time),
+			UnitOfMeasurement: record["unit_of_measurement"].(string),
+			Value:             record["value"].(string),
+			Identifier:        record["identifier"].(string),
+			Slug:              record["slug"].(string),
 		}); err != nil {
 			return err
 		}
